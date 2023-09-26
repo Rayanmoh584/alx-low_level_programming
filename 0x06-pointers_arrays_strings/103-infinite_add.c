be0606de6c265329fec58adcd4fcb8513830a727
Rayanmoh584 <95593157+Rayanmoh584@users.noreply.github.com>
2023-09-26 22:37:41 +0200

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,8 +12,7 @@ char temp;
 while (*(n + i) != '\0')
 {
 i++;
-}
-i--;
+} i--;
 for (j = 0; j < i; j++, i--)
 {
 temp = *(n + j);
@@ -46,11 +45,11 @@ while (j >= 0 || i >= 0 || overflow == 1)
 if (i < 0)
 val1 = 0;
 else
-val1 = *(n1 + i) - '0';
+val1 = *(n1 + i) -'0';
 if (j < 0)
 val2 = 0;
 else
-val2 = *(n2 + j) - '0';
+val2 = *(n2 + j) -'0';
 temp_tot = val1 + val2 + overflow;
 if (temp_tot >= 10)
 overflow = 1;
@@ -63,7 +62,8 @@ digits++;
 j--;
 i--;
 }
-if (digits == size_r)
+if
+(digits == size_r)
 return (0);
 *(r + digits) = '\0';
 rev_string(r);
